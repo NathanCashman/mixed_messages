2780ce04766f593afb49e628269c1cea81882f9e
Nathan Cashman <39572037+NathanCashman@users.noreply.github.com>
2021-02-11 23:06:22 +0000

    App is working, although basic. Could extend and add further functionality.

--- main.js
+++ main.js
@@ -5,4 +5,20 @@ make this dynamic in the future, via use of APIs which would allow the applicati
 update its pool of messages from 3rd party sources.
 */
 
-console.log('Test to confirm application is running');
+// Store message components
+
+const message = [['Money', 'Happiness', 'Love'], ['is', 'will always be', 'makes'], ['life', 'everything', 'what you make it']];
+
+// Generate random message
+
+const generateMessage = () => {
+    const mPart1 = message[0][Math.floor(Math.random() * 3)];
+    const mPart2 = message[1][Math.floor(Math.random() * 3)];
+    const mPart3 = message[2][Math.floor(Math.random() * 3)];
+
+    return `Today's message is: ${mPart1} ${mPart2} ${mPart3}. Run again to generate a new message!`;
+}
+
+// Test message generation
+
+console.log(generateMessage());
