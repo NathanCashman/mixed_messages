e2cf0f4cbc660697bf50d45095a00ea363d3c678
Nathan Cashman <39572037+NathanCashman@users.noreply.github.com>
2021-02-11 00:15:47 +0000

    nothing to see here

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+The Mixed Messages application generates a random message to the user each time it is run.
+The messages are all taken from a pool that is static, but potentially there is room to
+make this dynamic in the future, via use of APIs which would allow the application to
+update its pool of messages from 3rd party sources.
+*/
+
+console.log('Test to confirm application is running');
